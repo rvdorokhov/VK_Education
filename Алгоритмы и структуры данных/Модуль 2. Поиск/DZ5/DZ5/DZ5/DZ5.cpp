71954ccdd876cfc233da0afb1249d25c5e7e881e
rvdorokhov <theonlywaytotravel@mail.ru>
2024-07-09 13:46:36 +0300

    Решена задача А ДЗ5-2

---
+++ Алгоритмы и структуры данных/Модуль 2. Поиск/DZ5/DZ5/DZ5/DZ5.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stack>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    string str, result;
+    deque <char> S; char letter;
+
+    cin >> str;
+
+    S.push_back(str[0]);
+    for (unsigned int i = 1; i < str.size(); ++i) {
+        letter = str[i];
+        if (letter == S.back()) {
+            S.pop_back();
+        }
+        else {
+            S.push_back(letter);
+        }
+    }
+
+    unsigned int n = S.size(); str = "";
+    for (unsigned int i = 0; i < n; ++i) {
+        str += S.front();
+        S.pop_front();
+    }
+
+    //char* str1 = new char[str.size()];
+    //strcpy(str1, str.c_str());
+
+    cout << str.c_str() << endl;
+}

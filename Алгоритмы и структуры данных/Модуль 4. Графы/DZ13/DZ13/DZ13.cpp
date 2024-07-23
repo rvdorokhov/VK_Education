cf766c4529ef6eb6110f836ae88c15f90842666d
rvdorokhov <theonlywaytotravel@mail.ru>
2024-07-23 17:39:24 +0300

    Решена задача DZ13-1

--- Алгоритмы и структуры данных/Модуль 4. Графы/DZ13/DZ13/DZ13.cpp
+++ Алгоритмы и структуры данных/Модуль 4. Графы/DZ13/DZ13/DZ13.cpp
@@ -1,5 +1,7 @@
-﻿// DZ13.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿// 1) У вас есть граф, где каждая вершина представляет человека, а ребро между двумя вершинами означает, что эти два человека друзья. 
+//    Найдите, существуют ли три человека, которые все взаимно дружат друг с другом (то есть между каждой парой из трех человек существует ребро).
+// 2) Определите количество различных социальных кругов в графе. Социальный круг – это подграф, где все вершины связаны между собой напрямую или через других членов круга.
+// Решено на основе шаблона из директории "Шаблоны структур данных и алгоритмов"
 
 #include <iostream>
 #include <vector>
@@ -20,31 +22,9 @@ public:
         gr[vertex2].push_back(vertex1);
     }
 
-    void delete_edge(int vertex1, int vertex2) {
-        vertex1--; vertex2--;
-        gr[vertex1].remove(vertex2);
-        gr[vertex2].remove(vertex1);
-    }
-
     void add_vertex() {
         gr.resize(gr.size() + 1);
         visited.resize(visited.size() + 1);
-    }
-
-    void print_edges() {
-        for (int vertex = 0; vertex < gr.size(); ++vertex) {
-            std::cout << vertex + 1 << ": ";
-            for (auto neighbour : gr[vertex]) {
-                std::cout << neighbour + 1 << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    void testing(int vertex, int component) {
-        dfs(vertex, component);
-        for (int elem : visited) { std::cout << elem << " "; }
-        std::cout << std::endl;
     }
 
     int size() {
@@ -63,27 +43,23 @@ public:
         return visited;
     }
 
-    std::vector<int> get_component_of_vertex(int vertex) {
-        visited.assign(visited.size(), 0);
-        dfs(vertex);
-        return visited;
+    bool get_cycle() {
+        bool flag = false;
+        for (int vertex = 1; vertex < gr.size(); ++vertex) {
+            visited.assign(visited.size(), 0);
+            memory.clear();
+            flag = dfs_cycle_search(vertex - 1, vertex - 1, 1);
+            if (flag) { return flag; }
+        }
+        return flag;
     }
 
 private:
     std::vector<std::list<int>> gr;
     std::vector<int> visited;
+    std::vector<int> memory;
 
-    void dfs(int vertex) { // классический поиск в глубину
-        visited[vertex] = 1;
-
-        for (auto neighbour : gr[vertex]) {
-            if (!visited[neighbour]) {
-                dfs(neighbour, 1);
-            }
-        }
-    }
-
-    void dfs(int vertex, int component) { // выделение компонент связности
+    void dfs(int vertex, int component = 1) { // выделение компонент связности, при пустом параметре component классический поиск в глубину
         visited[vertex] = component;
 
         for (auto neighbour : gr[vertex]) {
@@ -93,22 +69,52 @@ private:
         }
     }
 
-    //void cycle_search(int vertex) { // поиск и восстановление цикла в графе
-    //    visited[vertex] = 0;
+    bool dfs_cycle_search(int start_vertex, int vertex, int count) { // поиск и восстановление цикла в графе
+        bool flag = false;
+        visited[vertex] = 1; // 1 - "вершина была посещена в текущем рекурсивном обходе"
+                             // 2 - "вершина была посещена ранее"
+        if (count == 4) { return false; }
 
-    //    for (auto neighbour : gr[vertex]) {
-    //        if (!visited[neighbour]) {
-    //            dfs(neighbour, 1);
-    //        }
-    //    }
-    //}
+        for (auto neighbour : gr[vertex]) {
+            if (!visited[neighbour]) {
+                flag = dfs_cycle_search(start_vertex, neighbour, count + 1);
+                if (flag) { return true; } // если мы нашли цикл, то завершаем функцию
+            }
+            else if ((visited[neighbour] == 1) && (neighbour == start_vertex) && (count == 3)) {
+                return true;
+            }
+        }
+
+        visited[vertex] = 2;
+
+        return flag;
+    }
 };
 
-
-int main()
-{
+void task1() {
     int n, v1, v2; std::cin >> n;
-    neograph graph; // с запасом
+    neograph graph;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> v1; std::cin >> v2;
+
+        while ((graph.size() < v1) || (graph.size() < v2)) {
+            graph.add_vertex();
+        }
+
+        graph.add_edge(v1, v2);
+    }
+
+    if (graph.get_cycle()) {
+        std::cout << "YES";
+    }
+    else {
+        std::cout << "NO";
+    }
+}
+
+void task2() {
+    int n, v1, v2; std::cin >> n;
+    neograph graph;
     for (int i = 0; i < n; ++i) {
         std::cin >> v1; std::cin >> v2;
 
@@ -127,8 +133,9 @@ int main()
     }
 
     std::cout << maxim;
-
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int main()
+{
+
+}

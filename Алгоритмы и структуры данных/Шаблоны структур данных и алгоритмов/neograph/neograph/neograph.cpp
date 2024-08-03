9f91709a329671bf50e0af58bef032f6174a0c3c
rvdorokhov <theonlywaytotravel@mail.ru>
2024-08-03 21:36:56 +0300

    Добавлена возможность присваивать ребрам вес

--- Алгоритмы и структуры данных/Шаблоны структур данных и алгоритмов/neograph/neograph/neograph.cpp
+++ Алгоритмы и структуры данных/Шаблоны структур данных и алгоритмов/neograph/neograph/neograph.cpp
@@ -4,7 +4,6 @@
 #include <stack>
 #include <queue>
 
-
 class Neograph {
 public:
     Neograph(int vertex = 1) {
@@ -13,19 +12,19 @@ public:
         visited.assign(visited.size(), 0);
     }
 
-    void add_edge(int vertex1, int vertex2) {
+    void add_edge(int vertex1, int vertex2, int weight = 1) {
         while ((gr.size() < vertex1) || (gr.size() < vertex2)) { // граф автмоатически дозаполняется, если была передана вершина с номером большим, чем размер графа
             add_vertex();
         }
         vertex1--; vertex2--;
-        gr[vertex1].push_back(vertex2);
-        gr[vertex2].push_back(vertex1);
+        gr[vertex1].push_back(std::make_pair(vertex2, weight));
+        gr[vertex2].push_back(std::make_pair(vertex1, weight));
     }
 
     void delete_edge(int vertex1, int vertex2) {
         vertex1--; vertex2--;
-        gr[vertex1].remove(vertex2);
-        gr[vertex2].remove(vertex1);
+        gr[vertex1].remove_if([=](std::pair<int, int> edge) { return edge.first == vertex2; });  //vertex2
+        gr[vertex2].remove_if([=](std::pair<int, int> edge) { return edge.first == vertex1; });  //vertex1
     }
 
     void add_vertex() {
@@ -37,7 +36,7 @@ public:
         for (int vertex = 0; vertex < gr.size(); ++vertex) {
             std::cout << vertex + 1 << ": ";
             for (auto neighbour : gr[vertex]) {
-                std::cout << neighbour + 1 << " ";
+                std::cout << neighbour.first + 1 << " ";
             }
             std::cout << std::endl;
         }
@@ -101,25 +100,17 @@ public:
         return visited;
     }
 
-    std::vector<int> get_bfs() {
+    std::vector<int> get_bfs(int vertex = 1) {
         visited.assign(visited.size(), INF);
-        for (int vertex = 0; vertex < visited.size(); ++vertex) {
-            if (visited[vertex] == INF) {
-                bfs(vertex);
-            }
-        }
-        return visited;
-    }
-
-    std::vector<int> get_bfs(int vertex) {
-        visited.assign(visited.size(), INF);
-        bfs(vertex);
+        bfs(vertex - 1);
         return visited;
     }
 
 protected:
     const int INF = 10e9;           // константа условной бесконечности
-    std::vector<std::list<int>> gr; // содержит информацию о связях в вершинах
+    std::vector<std::list<
+        std::pair<int, int>>        // <номер вершины, вес ведущего в него ребра>
+        > gr;                       // содержит информацию о связях в вершинах
     std::vector<int> visited;       // используются в функциях и процедурах для хранения промежуточных и итоговых значений
     std::vector<int> memory;
 
@@ -128,8 +119,8 @@ protected:
         visited[vertex] = component;
 
         for (auto neighbour : gr[vertex]) {
-            if (!visited[neighbour]) {
-                dfs(neighbour, component);
+            if (!visited[neighbour.first]) {
+                dfs(neighbour.first, component);
             }
         }
 
@@ -142,34 +133,17 @@ protected:
         memory.push_back(vertex);
 
         for (auto neighbour : gr[vertex]) {
-            if (!visited[neighbour]) {
-                flag = dfs_cycle_search(start_vertex, neighbour, vertex);
+            if (!visited[neighbour.first]) {
+                flag = dfs_cycle_search(start_vertex, neighbour.first, vertex);
                 if (flag) { return true; } // если мы нашли цикл, то завершаем функцию
             }
-            else if ((visited[neighbour] == 1) && (neighbour != parent) && (neighbour == start_vertex)) {
+            else if ((visited[neighbour.first] == 1) && (neighbour.first != parent) && (neighbour.first == start_vertex)) {
                 return true;
             }
         }
 
         memory.pop_back();
         visited[vertex] = 2;
-
-        return flag;
-    }
-
-    bool dfs_bipartite(int vertex, int colour) {
-        visited[vertex] = colour; bool flag = true;
-
-        for (auto neighbour : gr[vertex]) {
-            if (!visited[neighbour]) {
-                flag = dfs_bipartite(neighbour, 3 - colour); // если пометить цвета как 1 и 2, то 3 - 1 = 2 и 3 - 2 = 1, 
-                                                             // что очень удобно при передачи в рекурсивный вызов функции
-                if (!flag) { return false; }
-            }
-            else if (visited[neighbour] == colour) {
-                return false;
-            }
-        }
 
         return flag;
     }
@@ -183,13 +157,30 @@ protected:
         while (!q.empty()) {
             cur_vertex = q.front();
             q.pop();
-            for (int vertex : gr[cur_vertex]) {
-                if (visited[vertex] > visited[cur_vertex] + 1) {
-                    visited[vertex] = visited[cur_vertex] + 1;
-                    q.push(vertex);
+            for (std::pair<int, int> vertex : gr[cur_vertex]) {
+                if (visited[vertex.first] > visited[cur_vertex] + 1) {
+                    visited[vertex.first] = visited[cur_vertex] + 1;
+                    q.push(vertex.first);
                 }
             }
         }
+    }
+private:
+    bool dfs_bipartite(int vertex, int colour) {
+        visited[vertex] = colour; bool flag = true;
+
+        for (auto neighbour : gr[vertex]) {
+            if (!visited[neighbour.first]) {
+                flag = dfs_bipartite(neighbour.first, 3 - colour); // если пометить цвета как 1 и 2, то 3 - 1 = 2 и 3 - 2 = 1, 
+                // что очень удобно при передачи в рекурсивный вызов функции
+                if (!flag) { return false; }
+            }
+            else if (visited[neighbour.first] == colour) {
+                return false;
+            }
+        }
+
+        return flag;
     }
 };
 
@@ -197,25 +188,25 @@ class Ograph : public Neograph {
 public:
     Ograph(int vertex = 1) : Neograph(vertex) { }
 
-    void add_edge(int vertex1, int vertex2) {
+    void add_edge(int vertex1, int vertex2, int weight = 1) {
         while ((gr.size() < vertex1) || (gr.size() < vertex2)) { // граф автмоатически дозаполняется, если была передана вершина с номером большим, чем размер графа
             add_vertex();
         }
         vertex1--; vertex2--;
-        gr[vertex1].push_back(vertex2);
+        gr[vertex1].push_back(std::make_pair(vertex2, weight));
     }
 
     void delete_edge(int vertex1, int vertex2) {
         vertex1--; vertex2--;
-        gr[vertex1].remove(vertex2);
+        gr[vertex1].remove_if([=](std::pair<int, int> edge) { return edge.first == vertex2; });
     }
 
-    std::vector<int> dfs_bipartite(int vertex, int colour) {
+    std::vector<int> bipartite_parts() {
         // тут надо сначала построить вспомогательный неограф, затем вызвать от него функцию определения двудольности родительского класса
         Neograph neogr(gr.size()); // вспомогательный неориентированный граф
         for (int i = 0; i < gr.size(); ++i) { // заполняем вспомогательный неориентированный граф
-            for (int elem : gr[i]) {
-                neogr.add_edge(i + 1, elem + 1);
+            for (std::pair<int, int> edge : gr[i]) {
+                neogr.add_edge(i + 1, edge.first + 1);
             }
         }
 
@@ -239,9 +230,9 @@ public:
 
     std::vector<int> get_components() { // Алгоритм Косараджу-Шарира
         Ograph inverted_gr(gr.size()); // вспомогательный инверитрованный граф
-        for (int i = 0; i < gr.size(); ++i) { // заполняем вспомогательный инверитрованный граф
-            for (int elem : gr[i]) {
-                inverted_gr.add_edge(elem + 1, i + 1);
+        for (int i = 0; i < gr.size(); ++i) { // заполняем вспомогательный неориентированный граф
+            for (std::pair<int, int> edge : gr[i]) {
+                inverted_gr.add_edge(i + 1, edge.first + 1);
             }
         }
         memory = inverted_gr.get_topologic();   // запускаем "топлогическую" сортировку от каждой непосещенной вершины (на случай если граф несвязный)
@@ -263,8 +254,8 @@ private:
         visited[vertex] = 1;
 
         for (auto neighbour : gr[vertex]) {
-            if (!visited[neighbour]) {
-                dfs_topologic(neighbour);
+            if (!visited[neighbour.first]) {
+                dfs_topologic(neighbour.first);
             }
         }
 
@@ -274,7 +265,7 @@ private:
 
 int main()
 {
-    Neograph graph;
+    Ograph graph;
 
     graph.add_edge(1, 2); graph.add_edge(1, 4);
     graph.add_edge(2, 9); 
@@ -284,7 +275,9 @@ int main()
     graph.add_edge(4, 8); graph.add_edge(4, 10);
     graph.add_edge(8, 10);
 
-    std::vector<int> result = graph.get_bfs();
+    std::vector<int> result = graph.get_topologic();
+    result = graph.get_components();
+
 
     for (int elem : result) {
         std::cout << elem << " ";
